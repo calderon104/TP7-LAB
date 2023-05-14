4a9847f12120e53a625b3c6768a74aa1c4648a15
calderon104 <gonza.rc1004@gmail.com>
2023-05-14 17:58:34 -0300

    Colegio

---
+++ TP-7-LABO/src/tp/pkg7/labo/Colegio.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp.pkg7.labo;
+
+/**
+ *
+ * @author Gonza
+ */
+public class Colegio {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Materia m = new Materia(1,"Ingles",1);
+       
+    }
+    
+}
